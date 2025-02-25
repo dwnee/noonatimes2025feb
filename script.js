cf96c53ccd980898e8b4f77786d7223176ba512f
Dawon Jung <dwnee0@gmail.com>
2025-02-25 23:22:33 +0900

    url change

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let news = []
 const getLatestNews = async() =>{
-  const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?country=us`)
+  // const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?country=us&page=1&pageSize=20`)
+  // // https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?page=1&pageSize=20
+  const url = new URL(
+    `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=20`
+  );
   console.log("uuu", url)
   const response = await fetch(url)
   const data = await response.json()
