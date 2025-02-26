9b9b69229eeb0a3b13a541f11938976a39f24b83
Dawon Jung <dwnee0@gmail.com>
2025-02-26 16:16:05 +0900

    api url change

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let news = []
 const getLatestNews = async() =>{
   const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?country=us&page=1&pageSize=20`)
+  // https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?q=아이유
   // https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?page=1&pageSize=20
   // const url = new URL(
   //   `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&pageSize=20`
