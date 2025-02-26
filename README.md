fd403d6174aeca095ac682de97cc0a7c27234593
Dawon Jung <dwnee0@gmail.com>
2025-02-26 16:36:56 +0900

    url change change

---
+++ README.md
@@ -0,0 +1,1 @@
+https://noonatimes111.netlify.app/
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let news = []
 const getLatestNews = async() =>{
-  const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?country=us&page=1&pageSize=20`)
+  // const url = new URL(`https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?page=1&pageSize=20`)
+  const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`)
   // https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?q=아이유
   // https://study-website-be-bbb1539aa813.herokuapp.com/top-headlines?page=1&pageSize=20
   // const url = new URL(
@@ -8,6 +9,7 @@ const getLatestNews = async() =>{
   // );
   console.log("uuu", url)
   const response = await fetch(url)
+  console.log(response)
   const data = await response.json()
   news = data.articles
   console.log("Ddd", data.articles)
